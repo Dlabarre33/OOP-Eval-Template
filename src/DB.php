7421a9dcc89d8c62170b61a51c4cbb6e82885976
David L <dlabarre33@gmail.com>
2021-12-03 11:34:15 +0100

    dump autoload

--- src/DB.php
+++ src/DB.php
@@ -6,21 +6,21 @@ namespace App;
 
 use PDO;
 
-    class DB
+class DB
+{
+    private static PDO $pdo;
+
+    public function __construct(string $config = 'mysql:host=db;dbname=oop;', string $user = 'root', string $password = 'root')
     {
-        private static PDO $pdo;
-
-        public function __construct(string $config = 'mysql:host=HOST;dbname=DATABASE;', string $user = 'USERNAME', string $password = 'PASSWORD')
-        {
-            try {
-                static::$pdo = new PDO($config, $user, $password);
-            } catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), $e->getCode());
-            }
-        }
-
-        public function getStaticPdo(): PDO
-        {
-            return static::$pdo;
+        try {
+            static::$pdo = new PDO($config, $user, $password);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), $e->getCode());
         }
     }
+
+    public function getStaticPdo(): PDO
+    {
+        return static::$pdo;
+    }
+}
